e20e761f8036ac2db3d2b603108cd86ed60a45f0
Νικόλαος Κυριάκος Φυτίλης <n-fit@live.com>
2016-09-22 11:29:10 +0300

    1.16

--- reset.sh
+++ reset.sh
@@ -2,6 +2,6 @@ ver=$1
 
 for i in */PKGBUILD ; do
   sed -i -e "s/pkgver=\${_ver}.*/pkgver=\${_ver}.0/g" $i
-  sed -i -e "s/=1.15/=$ver/g" $i
+  sed -i -e "s/=1.17/=$ver/g" $i
   sed -i -e "s/pkgrel=.*/pkgrel=1/g" $i
 done
