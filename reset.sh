94a2c3a91c9faff86f5e5fab69b139fc9985c2b6
Νικόλαος Κυριάκος Φυτίλης <n-fit@live.com>
2016-11-21 15:06:23 +0200

    Is this real life?

--- reset.sh
+++ reset.sh
@@ -4,6 +4,6 @@ zsh -c 'echo -n > ~/.cache/notify-*/SHA1SUMS.old'
 
 for i in atril/PKGBUILD ; do
   sed -i -e "s/pkgver=\${_ver}.*/pkgver=\${_ver}.0/g" $i
-  sed -i -e "s/=1.1.*/=$ver/g" $i
+  sed -i -e "s/=1.16./=$ver/g" $i
   sed -i -e "s/pkgrel=.*/pkgrel=1/g" $i
 done
