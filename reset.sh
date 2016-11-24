78c122b3f0f0292934e692f10201715e29a86431
Νικόλαος Κυριάκος Φυτίλης <n-fit@live.com>
2016-11-24 16:14:46 +0200

    reset shas

--- reset.sh
+++ reset.sh
@@ -2,6 +2,11 @@ ver=$1
 
 zsh -c 'echo -n > ~/.cache/notify-*/SHA1SUMS.old'
 
+reset_sha() {
+  a=$(cat $@ | grep sha1 | sed -e 's/sha1sums=//' | tr -d \'\(\)\" )
+  sed -e "s/$a/123123321/g" -i $@
+}
+
 for i in */PKGBUILD ; do
   sed -i -e "s/pkgver=\${_ver}.*/pkgver=\${_ver}.0/g" $i
   sed -i -e "s/1.16/$ver/g" $i
