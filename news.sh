93cfcec9a31af44befa558a8448d0766d8228dd1
Νικόλαος Κυριάκος Φυτίλης <n-fit@live.com>
2016-12-21 12:37:49 +0200

    new script for mate news - creates a file with the diff of the news and 2 folders in the .. dir

---
+++ news.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/zsh
+declare -a news_array
+
+firstrun=1
+
+if [ -e ../NEWS ]
+  then rm -rf ../NEWS.old ; mv ../NEWS ../NEWS.old ; firstrun=0
+fi
+
+mkdir ../NEWS
+
+cd ../NEWS
+
+for i in ../arch_mate/*/ 
+  do news_array=( $(echo ${i/arch_mate} | tr -d './') ${news_array[@]} )
+done
+
+#hacks
+news_array=( caja-extensions ${news_array[@]##caja-extensions-common} )
+
+for i in ${news_array[@]}
+  do curl https://raw.githubusercontent.com/mate-desktop/$i/master/NEWS > $i
+  if [ "$firstrun" -eq '0' ]
+    then news=$(diff $i ../NEWS.old/$i | sed -e '1 d' | sed -e 's/^. //g')
+    [ -z "$news" ] || echo -e $i':\n'"$news">> ../arch_mate/NEWS
+  fi
+done
